d0e9cd42cdbeb8624411724284394d7959019506
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-09-25 16:31:10 +0300

    Create 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers.
+ * @a: First integer to swap.
+ * @b: Second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order.
+ * @array: Array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t j, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (j = 0; j < len - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_ints(array + j, array + j + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
